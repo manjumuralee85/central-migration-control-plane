8a69350ae3a02fce6bad691f2d424cb10984656d
Manju Murali <manju.muralee@ibsplc.com>
2026-02-25 14:05:49 +0530

    Centralised templates-build correction

--- templates/repo-patches/spring-petclinic/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ templates/repo-patches/spring-petclinic/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath*:spring/*.xml")
 public class PetClinicApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
